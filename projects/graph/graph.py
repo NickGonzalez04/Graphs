79b9e3f9bdc77072673c8e4b3c33bcf62a078307
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-02-18 14:08:52 -0500

    Graph_guided/Word_ladder

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -29,7 +29,9 @@ class Graph:
         """
         Get all neighbors (edges) of a vertex.
         """
+        # print('neighbor',self.vertices[vertex_id])
         return self.vertices[vertex_id]
+        
 
     def bft(self, starting_vertex):
         """
@@ -37,26 +39,26 @@ class Graph:
         beginning from starting_vertex.
         """
         # Create an empty queue 
+        q = Queue()
         # Add the starting vertex_id to the queue
+        q.enqueue(starting_vertex)
         # Create an empty set to stored visited nodes
+        visited = set()
         # While the queue is not empty ...
+        while q.size() > 0:
             # Dequeue, the first vertex
+            v = q.dequeue()
             # Check if it's been visited
             # If it has not been visited ....
-                # Mark it has been visited
-                # Then add all neighbors to the back of the queue
-        q = Queue()
-        q.enqueue(starting_vertex)
-        visited = set()
-        while q.size() > 0:
-            v = q.dequeue()
             if v not in visited:
                 print(v)
+                # Mark it has been visited
                 visited.add(v)
-
+                # Then add all neighbors to the back of the queue
                 for neighbor in self.get_neighbors(v):
                     q.enqueue(neighbor)
-        print("-----------")
+        print("bft-----------")
+
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
@@ -71,44 +73,100 @@ class Graph:
         # While the stack is not empty ...
         while s.size() > 0:
             # Pop, the first vertex
-            v= s.pop()
+            v = s.pop()
             # Check if it's been visited
             # If it has not been visited ....
             if v not in visited:
                 # Mark it has been visited
-                
                 print(v)
                 visited.add(v)
                 # Then push all neighbors to the back of the queue
                 for neighbor in self.get_neighbors(v):
                     s.push(neighbor)
-
-    def dft_recursive(self, starting_vertex):
+        print("dft------------")
+    def dft_recursive(self, starting_vertex, visited=None):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
 
         This should be done using recursion.
         """
-        pass  # TODO
+        # Check if node is visited
+        if visited is None:
+            visited = set()
+        # Setbase case 
+        # if not visited
+        if starting_vertex not in visited:
+            visited.add(starting_vertex)
+            # Make as visited
+            # Print
+            print(starting_vertex)
+            # Call Dft_Recursive on each child 
+            for neighbor in self.get_neighbors(starting_vertex):
+                self.dft_recursive(neighbor, visited)
+        print("dft_recursive------------")
+
+                
 
     def bfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing the shortest path from
-        starting_vertex to destination_vertex in
+        starting_vertex to destination_vertex (target) in
         breath-first order.
         """
-        pass  # TODO
-
+        q = Queue()
+        q.enqueue( [starting_vertex] )
+        visited = set()
+        while q.size() > 0:
+            path = q.dequeue()
+            v = path[-1]
+            if v == destination_vertex:
+                return path
+            if v not in visited:
+                visited.add(v)
+                for neighbor in self.get_neighbors(v):
+                    path_copy = path.copy()
+                    path_copy.append(neighbor)
+                    q.enqueue(path_copy)
+        print("bfs------------")
     def dfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        pass  # TODO
+        s = Stack()
+        # Add A PATH TO the starting vertex_id to the stack
+        s.push( [starting_vertex] )
+        # Create an empty set to store visited nodes
+        visited = set()
+        # While the stack is not empty...
+        counter = 0
+        while s.size() > 0:
+            counter += 1
+            print(counter)
+            # Pop, the first PATH
+            path = s.pop()
+            # GRAB THE LAST VERTEX FROM THE PATH
+            v = path[-1]
+            # CHECK IF IT'S THE TARGET
+            if v == destination_vertex:
+                # IF SO, RETURN THE PATH
+                return path
+            # Check if it's been visited
+            # If it has not been visited...
+            if v not in visited:
+                # Mark it as visited
+                visited.add(v)
+                # Then add A PATH TO all neighbors to the top of the stack
+                for neighbor in self.get_neighbors(v):
+                    # (Make a copy of the path before adding)
+                    path_copy = path.copy()
+                    path_copy.append(neighbor)
+                    s.push(path_copy)
 
-    def dfs_recursive(self, starting_vertex):
+
+    def dfs_recursive(self, starting_vertex, destination_vertex, visited=None, path=None):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
@@ -116,7 +174,27 @@ class Graph:
 
         This should be done using recursion.
         """
-        pass  # TODO
+        if visited is None:
+            visited = set()
+        if path is None:
+            path = []
+        # Check if starting vertex has been visited
+        if starting_vertex not in visited:
+        # If not ...
+            visited.add(starting_vertex)
+            path_copy = path.copy()
+            path_copy.append(starting_vertex)
+            # If starting_vertex is destination:
+            if starting_vertex == destination_vertex:
+                return path_copy
+                # return path
+            # Mark as visited
+            # Call DFS recursive on each neighbor
+            for neighbor in self.get_neighbors(starting_vertex):
+                new_path = self.dfs_recursive(neighbor, destination_vertex, visited, path_copy)
+                if new_path is not None:
+                    return new_path
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
---
+++ projects/graph/word_ladder.py
@@ -0,0 +1,62 @@
+from util import Stack, Queue
+
+
+
+f = open('words.txt', 'r')
+words = f.read().split("\n")
+f.close()
+
+word_set = set([w.lower() for w in words])
+
+def get_neighbors(w):
+    '''
+    Return all words in word_set that have 1 and only 
+    '''
+    # For each letter in the word
+    neighbors = []
+    alphabet = ['a','b','c','d', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q','r','s','t','u','v','w','x','y', 'z']
+    letter_list = list(w)
+    for i in range(len(letter_list)):
+        # For each letter in the alphabet
+        for letter in alphabet:
+            # Replace the word letter with the alphabet letter
+            temp_word = list(letter_list)
+            temp_word[i] = letter
+            new_word = "".join(temp_word)
+            # Check if the resulting word is in the word_set
+            # (Ignore if equal to the original word)
+            if new_word in word_set and new_word != w:
+                # If so, add to neighbor list
+                neighbors.append(new_word)
+    return neighbors
+
+        # For each letter in the alphabet
+        # Replace the word letter with the alphabet
+        # Check if the resulting word is in the word_set
+        # If so, add to neighbor list
+
+def find_ladders(begin_word, end_word):
+    q = Queue()
+
+    q.enqueue( [begin_word])
+    visited = set()
+
+    while q.size() > 0:
+        path = q.dequeue()
+
+        w= path[-1]
+
+        if w == end_word:
+            return path
+
+        if w not in visited:
+            visited.add(w)
+
+            for neighbor in get_neighbors(w):
+                path_copy = path.copy()
+                path_copy.append(neighbor)
+                q.enqueue(path_copy)
+    return None
+
+
+print(find_ladders('bat', 'zoo'))
